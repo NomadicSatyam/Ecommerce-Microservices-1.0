bfeaa0110c71ec108833eebdac8720b616326606
Satyam Singh <Satyamm1998@gmail.com>
2024-02-05 11:34:41 +0530

    initial commit

---
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.orderservice.controller;
+
+
+import com.ecommerce.orderservice.dto.OrderRequest;
+import com.ecommerce.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -0,0 +1,77 @@
+package com.ecommerce.orderservice.service;
+import com.ecommerce.orderservice.dto.InventoryResponse;
+import com.ecommerce.orderservice.dto.OrderLineItemsDto;
+import com.ecommerce.orderservice.dto.OrderRequest;
+import com.ecommerce.orderservice.model.Order;
+import com.ecommerce.orderservice.model.OrderLineItems;
+import com.ecommerce.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
+
+    public void placeOrder(OrderRequest orderRequest)
+    {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                                     .map(OrderLineItems::getSkuCode)
+                                     .toList();
+
+        // Call Inventory Service and placed order
+        // if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder
+                .build()
+                .get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                                           .allMatch(InventoryResponse::isInStock);
+
+       if(allProductsInStock)
+       {
+          orderRepository.save(order);
+       }
+       else
+       {
+           throw new IllegalArgumentException("Product is not in stock,Please try again later");
+       }
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto)
+    {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
